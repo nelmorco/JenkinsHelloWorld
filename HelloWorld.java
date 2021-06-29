888ae8a069ea852008d09c74a08dda54be9e5ee6
nelmorco <83439773+nelmorco@users.noreply.github.com>
2021-06-29 16:51:25 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World welcome to Jenkins (Changes)");
+        System.out.println("Hello World welcome to Jenkins, We have some Changes");
     }
 }
